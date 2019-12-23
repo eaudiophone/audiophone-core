e15bacde980987b992cfa8e554d87a09550ea533
gabmart1995 <gabmart1995@gmail.com>
2019-12-23 17:24:28 -0400

    wip: diseño de pages para react-router, configuracion del enviroment

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 
 // componentes
-// import Navbar from './../../components/navbar/Navbar';
-// import Sidebar from './../../components/sidebar/Sidebar'
+import Navbar from './../../components/navbar/Navbar';
+import Sidebar from './../../components/sidebar/Sidebar'
 
 class Home extends Component {
 
 	render() {
+
 		return (
 
 			<div>
-				<h2>Home works</h2>
+				<Navbar />
+				<Sidebar />
 			</div>
 		);
 	}
