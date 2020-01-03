51f3055ea363991fa8fb4bbefe18e12f6ce0ddba
gabmart1995 <gabmart1995@gmail.com>
2020-01-02 23:12:14 -0400

    wip

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,58 +3,48 @@ import React from 'react';
 import 'bootstrap/js/dist/collapse';
 import './Navbar.css';
 
-class Navbar extends React.Component {
+const Navbar = () => (
 
-	render() {
-		
-		return (
-	
-			<div className="navbar-expand-sm">
-				<nav 
-					role="navigation" 
-					className="navbar navbar-dark bg-dark"
-				>
-					<span className="navbar-brand">Audiophone</span>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-toggle="collapse"
-						data-target="#navbarNav"
-						aria-controls="navbarNav"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					{ this.getMenu() }
-          		</nav>
-			</div>
-		);
-	}
-
-	getMenu() {
-
-		return (
-
-			<div 
-				className="collapse navbar-collapse justify-content-end"
-				id="navbarNav"
+	<div className="navbar-expand-sm">
+		<nav 
+			role="navigation" 
+			className="navbar navbar-dark bg-dark"
+		>
+			<span className="navbar-brand">Audiophone</span>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
 			>
-				<ul className="navbar-nav">
-            		<li className="nav-item">
-            			<span className="nav-link">Notifications</span>
-            		</li>
-            		<li className="nav-item">
-            			<span className="nav-link">User</span>
-            		</li>
-            		<li className="nav-item">
-            			<a className="nav-link" href="/">Sign out</a>
-            		</li>
-          		</ul>
-			</div>	
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			{ getMenu() }
+         </nav>
+	</div>
+);
 
-		);
-	}
-}
+const getMenu = () => (
+
+	<div 
+		className="collapse navbar-collapse justify-content-end"
+		id="navbarNav"
+	>
+		<ul className="navbar-nav">
+            <li className="nav-item">
+            	<span className="nav-link">Notifications</span>
+            </li>
+            <li className="nav-item">
+            	<span className="nav-link">User</span>
+            </li>
+            <li className="nav-item">
+            	<a className="nav-link" href="/">Sign out</a>
+            </li>
+        </ul>
+	</div>	
+);
 
 export default Navbar;
