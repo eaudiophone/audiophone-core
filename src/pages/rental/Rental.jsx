fd380fe81705cbd6dd9beed08aefc56e78bbafc7
gabmart1995 <gabmart1995@gmail.com>
2020-01-08 21:41:08 -0400

    maquetacion de equipos actualizada

--- src/pages/rental/Rental.jsx
+++ src/pages/rental/Rental.jsx
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import { rentalMeetings } from './RentalHardcode';
+
+import Card from './../../components/card/Card';
 
 class Rental extends Component {
 
+	constructor( props ) {
+
+		super( props );
+		this.state = { meetings: rentalMeetings };
+	}
+
+	setHeader() {
+
+		return (
+
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap 
+				align-items-center pb-2 mb-3 border-bottom">
+				
+				<h2>Alquiler de equipos</h2> 
+
+				<div className="btn-toolbar mb-2 mb-md-0">
+					<div className="btn-group">
+						<button className="btn btn-sm btn-success">
+							<i className="mr-2 fas fa-plus"></i>
+							Nuevo evento
+						</button>
+						<button className="btn btn-sm btn-secondary">
+							<i className="mr-2 fas fa-info-circle"></i>
+							Información
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	setMeetingRental() {
+
+		return (
+			<div className="row">
+				{ this.state.meetings.map( ( element ) => (
+					<Card meeting={ element } color="#fbf096" key={ element.id } />
+					)) }
+			</div>
+		);
+	}
+
 	render() {
-		return ( <h2>Rental works</h2> );
+		return ( 
+			<div>
+				{ this.setHeader() }
+				{ this.setMeetingRental() }
+			</div>
+		);
 	}
 }
 
