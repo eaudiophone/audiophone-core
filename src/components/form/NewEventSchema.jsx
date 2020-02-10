efc9cf5de269353eda1f75b8da7e2a4f2a0c3313
Gabriel Martínez <gabmart1995@gmail.com>
2020-02-09 21:48:41 -0400

    wip: se agrega el esquema de eventos

---
+++ src/components/form/NewEventSchema.jsx
@@ -0,0 +1,67 @@
+import { string, object, number, mixed, date } from 'yup';
+
+class NewEventSchema {
+
+	constructor() {
+
+		this.validationMessages = {
+			string: {
+				required: 'campo requerido',
+				min: (min) => `minimo ${ min } caracteres`,
+				max: (max) => `minimo ${ max } caracteres`
+			},
+			date: {
+				required: 'campo requerido'
+			},
+			mixed: {
+				required: 'campo requerido'
+			},
+			number: {
+				required: 'campo requerido'
+			}
+		};
+
+		this.setSchema();
+	}
+
+	setSchema() {
+
+		this.schema = object().shape({
+
+			title: string()
+				.required( this.validationMessages.string.required )
+				.min( 2, this.validationMessages.string.min( 2 ) )
+				.max( 25, this.validationMessages.string.max( 25 ) ),
+
+			idService: number()
+				.required( this.validationMessages.number.required ),
+
+			date: date()
+				.required( this.validationMessages.date.required ),
+
+			startingTime: mixed()
+				.required( this.validationMessages.mixed.required ),
+
+			finalHour: mixed()
+				.required( this.validationMessages.mixed.required ),
+
+			totalHours: number()
+				.required( this.validationMessages.number.required ),
+
+			description: string()
+				.required( this.validationMessages.string.required )
+				.min( 10, this.validationMessages.string.min( 10 ) )
+				.max( 255, this.validationMessages.string.max( 255 ) )
+		});
+	}
+
+	getSchema() {
+		return this.schema;
+	}
+
+	async testData( form ) {
+		return await this.schema.isValid( form );
+	} 
+}
+
+export default NewEventSchema;
