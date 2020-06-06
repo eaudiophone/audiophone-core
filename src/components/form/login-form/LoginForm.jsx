cd33f2daf0ca86dd52d878a8fdfcf5b0ea64d38b
gabriel <gabmat1995@gmail.com>
2020-06-06 15:32:38 -0400

    wip: creacion de componente de login form

---
+++ src/components/form/login-form/LoginForm.jsx
@@ -0,0 +1,154 @@
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { Formik } from 'formik';
+
+import LoginSchema from './LoginSchema';
+import Login from './../../../models/LoginModels';
+
+const LoginForm = ({ loading = false, getFormData }) => {
+
+	return (
+		<Formik
+      initialValues={ new Login( localStorage.getItem('email') ) }
+      validationSchema={ new LoginSchema().getSchema() }
+      onSubmit={ getFormData }
+      validateOnChange={ false }
+  	>
+    	{({ handleSubmit, handleChange, handleReset, values, errors }) => (
+
+       	<Form 
+         	className="form-signin" 
+         	onSubmit={ handleSubmit }
+         	noValidate
+        > 
+
+          <h2 className="mb-3">Estudio Audiophone</h2>
+          <h3 className="mb-3 text-center">
+            Sign In
+          </h3>
+
+          <EmailInput 
+          	value={ values.audiophoneusers_email }
+          	handleChange={ handleChange }
+          	error={ errors.audiophoneusers_email }
+          />                  
+
+        	 <PasswordInput 
+          	value={ values.audiophoneusers_password }
+          	handleChange={ handleChange }
+          	error={ errors.audiophoneusers_password }
+          />
+
+          <CheckboxInput 
+          	value={ values.remember }
+          	handleChange={ handleChange }
+          />
+
+          <Buttons handleReset={ handleReset } loading={ loading } />
+
+          <Register />  
+          
+          <p className="mt-3 mb-3 text-muted text-center">
+            &copy; Audiophone 2018
+          </p>
+            
+         </Form>
+    )}
+  </Formik>
+	);
+}
+
+const Register = () =>  {
+  
+  return (
+     <div className="register"> 
+        <p>
+            Si no posees cuenta
+            <a href="/register">Registrese</a>
+        </p>
+    </div>
+  );
+}
+
+
+const EmailInput = ({ value, handleChange, error }) => (
+
+	<Form.Group>
+		<Form.Label>email:</Form.Label>
+		<Form.Control
+      name="audiophoneusers_email"
+      type="email"
+      value={ value }  
+			onChange={ handleChange }
+      isInvalid={ !!error } 
+		/>
+    <Form.Control.Feedback type="invalid">
+      { error }
+    </Form.Control.Feedback>
+	</Form.Group>
+);
+    
+
+const PasswordInput = ({ value, handleChange, error }) => (
+
+	<Form.Group>
+    <Form.Label>password:</Form.Label>
+		<Form.Control
+      name="audiophoneusers_password"
+			type="password"
+      onChange={ handleChange }
+      value={ value } 
+			isInvalid={ !!error } 
+	   />
+    <Form.Control.Feedback type="invalid">
+      { error }
+    </Form.Control.Feedback>
+  </Form.Group>
+);
+    
+
+const CheckboxInput = ({ value, handleChange }) => (
+    		 
+	 <Form.Group>
+    <Form.Check
+      id="remember"
+      name="remember"
+      type="checkbox"
+      label="remember me"
+      onChange={ handleChange }
+      value={ value }
+    /> 
+   </Form.Group>
+);
+    
+const Buttons = ({ handleReset, loading = false }) => (
+
+  <div>
+  	{ !loading && (
+  			<Button type="submit" variant="primary" size="lg" block>
+  				Iniciar sesión
+				</Button>
+  		) 
+  	}
+  	{
+  		loading && (
+  			<Button variant="primary" size="lg" block disabled>
+					<i className="fas fa-spinner fa-spin mr-2"></i>
+					Iniciar sesión
+				</Button>
+  		)
+  	}
+    
+    <Button
+      onClick={ handleReset }
+      variant="secondary"
+      size="lg"
+      block
+    >
+        Cancelar
+    </Button>
+  </div>
+);
+   
+
+export default LoginForm;
--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Form, Button } from 'react-bootstrap';
-import { Formik } from 'formik';
+import { Container } from 'react-bootstrap';
 
 import AuthService from './../../services/AuthService'; 
 import RedirectService  from './../../services/RedirectService';
 
-import LoginSchema from './LoginSchema';
-import Login from './../../models/LoginModels';
-
 import ToastComponent from './../../components/toasts/ToastComponent';
+import LoginForm from '../../components/form/login-form/LoginForm';
 
 import './LoginPage.css';
 
@@ -56,165 +53,26 @@ class LoginPage extends Component {
     }, 1000 );
 	}
 
-    getEmailInput( value, handleChange, error ) {
-    	
-    	return (
+	render() {
 
-    		<Form.Group>
-    			<Form.Label>email:</Form.Label>
-    			<Form.Control
-            name="audiophoneusers_email"
-            type="email"
-            value={ value }  
-    				onChange={ handleChange }
-            isInvalid={ !!error } 
-    			/>
-          <Form.Control.Feedback type="invalid">
-            { error }
-          </Form.Control.Feedback>
-    		</Form.Group>
-    	);
-    }
+  	return (
 
-    getPasswordInput( value, handleChange, error ) {
+      <Container className="container-login">
 
-    	return (
+        { this.state.redirect && ( <RedirectService route="/profile" /> ) }
+        
+        <ToastComponent 
+          showToast={ this.state.showToast } 
+          content={ this.message } 
+          context={ this.action }
+          onHide={ () => this.setState({ showToast: false }) }
+        />
 
-  		<Form.Group>
-        <Form.Label>password:</Form.Label>
-  			<Form.Control
-          name="audiophoneusers_password"
-  				type="password"
-          onChange={ handleChange }
-          value={ value } 
-  				isInvalid={ !!error } 
-  		   />
-         <Form.Control.Feedback type="invalid">
-           { error }
-         </Form.Control.Feedback>
-  	    </Form.Group>
-    	);
-    }
+        <LoginForm loading={ this.state.loading } getFormData={ this.getFormData } />
 
-    getCheckboxInput( value, handleChange ) {
-
-    	return (
-    		 
-    		 <Form.Group>
-          <Form.Check
-            id="remember"
-            name="remember"
-            type="checkbox"
-            label="remember me"
-            onChange={ handleChange }
-            value={ value }
-          /> 
-         </Form.Group>
-    	);
-    }
-
-    getRegister() {
-
-    	return (
-            
-    		 <div className="register"> 
-        		<p>
-            		Si no posees cuenta
-            		<a href="/register">Registrese</a>
-        		</p>
-    		</div>
-    	);
-    }
-
-    getButtons( handleReset ) {
-
-        return (
-
-          <div>
-            { this.showButton() }
-              
-              <Button
-                onClick={ handleReset }
-                variant="secondary"
-                size="lg"
-                block
-              >
-                  Cancelar
-              </Button>
-          </div>
-        );
-    }
-
-    showButton() {
-
-      if ( !this.state.loading ) {
-        return (
-          <Button type="submit" variant="primary" size="lg" block>
-            Iniciar sesión
-          </Button>
-        );
-      }
-
-      return (
-        <Button variant="primary" size="lg" block disabled>
-          <i className="fas fa-spinner fa-spin mr-2"></i>
-          Iniciar sesión
-        </Button>
-      );
-
-    }
-
-  	render() {
-
-    	return (
-
-        <Container className="container-login">
-
-          { this.state.redirect && ( <RedirectService route="/profile" /> ) }
-          
-          <ToastComponent 
-            showToast={ this.state.showToast } 
-            content={ this.message } 
-            context={ this.action }
-            onHide={ () => this.setState({ showToast: false }) }
-           />
-
-            <Formik
-                initialValues={ new Login( localStorage.getItem('email') ) }
-                validationSchema={ new LoginSchema().getSchema() }
-                onSubmit={ this.getFormData }
-                validateOnChange={ false }
-            >
-              {({ handleSubmit, handleChange, handleReset, values, errors }) => (
-
-                 <Form 
-                   className="form-signin" 
-                   onSubmit={ handleSubmit }
-                   noValidate
-                  > 
-
-                    <h2 className="mb-3">Estudio Audiophone</h2>
-                    <h3 className="mb-3 text-center">
-                      Sign In
-                    </h3>                  
-
-                    { this.getEmailInput( values.audiophoneusers_email, handleChange, errors.audiophoneusers_email ) }
-                    { this.getPasswordInput( values.audiophoneusers_password, handleChange, errors.audiophoneusers_password ) }
-                    { this.getCheckboxInput( values.remember, handleChange ) }
-                    { this.getButtons( handleReset ) }
-                    { this.getRegister() }
-
-                    <p className="mt-3 mb-3 text-muted text-center">
-                      &copy; Audiophone 2018
-                    </p>
-                      
-                   </Form>
-              )}
-            </Formik>
-
-        </Container>
-     	);
-  	}
+      </Container>
+   	);
+	}
 }
 
 export default LoginPage;
