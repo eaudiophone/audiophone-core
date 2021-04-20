6945865fbc78aa5ab4a6b66aacf05f7d0bc70a1a
gabriel <gabmat1995@gmail.com>
2021-04-19 21:21:17 -0400

    wip

--- src/enviroment.jsx
+++ src/enviroment.jsx
@@ -9,6 +9,6 @@ export const URL_SERVER = {
 export const CLIENT_SECRET = {
   	grant_type: "password",
   	client_id: "2",
-  	client_secret: "jkQw27gTEOsllRXVszOnc6aPZAdruewI1TKu0v9v",
+  	client_secret: "zggNlWUBxmxse7pC7oDfoUq4Qz4aHk9x9K3po7yl",
   	scope: ""
 };
