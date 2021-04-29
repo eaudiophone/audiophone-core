240028a060a32f9a91cade25ce4d0a0ae0c34b5f
gabriel <gabmat1995@gmail.com>
2021-04-28 21:56:10 -0400

    wip

--- src/components/form/login-form/LoginForm.jsx
+++ src/components/form/login-form/LoginForm.jsx
@@ -4,14 +4,11 @@ import PropTypes from 'prop-types';
 
 import { Button } from "react-bootstrap";
 import { Formik, Form, Field } from 'formik';
-
 import { EmailInput, PasswordInput, CheckboxInput } from '../FormComponent';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import LoginSchema from './LoginSchema';
 import Login from './../../../models/LoginModels';
-
 
 const LoginForm = ({ loading = false, getFormData }) => {
 
@@ -25,44 +22,17 @@ const LoginForm = ({ loading = false, getFormData }) => {
     	{ ({ values, handleReset, isValid, isSubmitting }) => {
 
         return (
-
-         	<Form
-           	className="form-signin"
-            noValidate
-          >
-
+         	<Form className="form-signin" noValidate>
             <h2 className="mb-3">Estudio Audiophone</h2>
-            <h3 className="mb-3 text-center">
-              Iniciar sesión
-            </h3>
-
-            <Field
-              type="email"
-              name="audiophoneusers_email"
-              component={ EmailInput }
-            />
-
-            <Field
-              type="password"
-              name="audiophoneusers_password"
-              component={ PasswordInput }
-            />
-
-            <Field
-              type="checkbox"
-              name="remember"
-              component={ CheckboxInput }
-              label="Recordar"
-            />
-
+            <h3 className="mb-3 text-center">Iniciar sesión</h3>
+						<Field type="email" name="audiophoneusers_email" component={ EmailInput } />
+            <Field type="password" name="audiophoneusers_password" component={ PasswordInput } />
+            <Field type="checkbox" name="remember" component={ CheckboxInput } label="Recordar" />
             <Buttons handleReset={ handleReset } disabled={ !isValid } loading={ isSubmitting } />
-
             <Register />
-
             <p className="mt-3 mb-3 text-center">
-              &copy; Audiophone { new Date().getFullYear() }
+							&copy; Audiophone { new Date().getFullYear() }
             </p>
-
            </Form>
           );
         }
@@ -70,10 +40,6 @@ const LoginForm = ({ loading = false, getFormData }) => {
     </Formik>
 	);
 }
-
-LoginForm.propTypes = {
-  getFormData: PropTypes.func.isRequired
-};
 
 const Register = () =>  {
 
@@ -90,33 +56,28 @@ const Register = () =>  {
 const Buttons = ({ handleReset, loading = false, disabled = true }) => {
 
   return (
+	  <div>
+	  	{ !loading && (
+	  			<Button type="submit" variant="primary" disabled={ disabled } size="lg" block>
+	  				Acceder
+					</Button>
+	  		)
+	  	}
+	  	{
+	  		loading && (
+	  			<Button variant="primary" size="lg" block disabled>
+						<FontAwesomeIcon icon="spinner" spin className="mr-2" />
+						Acceder
+					</Button>
+	  		)
+	  	}
 
-  <div>
-  	{ !loading && (
-  			<Button type="submit" variant="primary" disabled={ disabled } size="lg" block>
-  				Acceder
-				</Button>
-  		)
-  	}
-  	{
-  		loading && (
-  			<Button variant="primary" size="lg" block disabled>
-					<FontAwesomeIcon icon="spinner" spin className="mr-2" />
-					Acceder
-				</Button>
-  		)
-  	}
-
-    <Button
-      onClick={ handleReset }
-      variant="secondary"
-      size="lg"
-      block
-    >
-        Cancelar
-    </Button>
-  </div>
-)};
+	    <Button onClick={ handleReset } variant="secondary" size="lg" block>
+	        Cancelar
+	    </Button>
+	  </div>
+	)
+};
 
 Buttons.propTypes = {
   handleReset: PropTypes.func.isRequired,
@@ -124,5 +85,8 @@ Buttons.propTypes = {
   disabled: PropTypes.bool
 };
 
+LoginForm.propTypes = {
+  getFormData: PropTypes.func.isRequired
+};
 
 export default LoginForm;
